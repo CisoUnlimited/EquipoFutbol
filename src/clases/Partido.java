e078298581369ebfcf455abc1c45dd5e0b06a052
Manuel Zambudio Herrerías <manuel.zambudio.herrerias@gmail.com>
2024-02-11 19:53:38 +0100

    Segundo commit - Comienzo de Partido.java

--- src/clases/Partido.java
+++ src/clases/Partido.java
@@ -1,4 +1,10 @@
 package clases;
 
+import java.util.HashMap;
+
 public class Partido {
+    private String[] equipoLocal;
+    private String[] equipoVisitante;
+    private HashMap<String,HashMap<Integer,Integer>> informe;
+
 }
